72fdf5b3449b88f2c7df6dd7e39dc1c81241c5c6
phopps <pathopps1@gmail.com>
2022-03-15 21:03:33 -0400

    testing servers

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import {
@@ -75,7 +52,6 @@ function App() {
         </div>
       </Router>
     </ApolloProvider>
->>>>>>> feature/client
   );
 }
 
