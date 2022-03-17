08826535872a37d2517cfdf403563dde8c9990a1
Isaaccna <isaac.andrade1231@gmail.com>
2022-03-17 17:04:28 -0400

    product form component, profile page

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -3,10 +3,11 @@ import { useParams } from 'react-router-dom';
 import { Link } from "react-router-dom";
 // import CommentList from '../components/CommentList';
 // import CommentForm from '../components/CommentForm';
-import Auth from '../utils/auth';
+import Auth from '../../utils/auth';
 import { useQuery } from '@apollo/client';
-import { QUERY_PRODUCTS } from '../utils/queries';
-const SingleProduct = (props) => {
+import { QUERY_PRODUCTS } from '../../utils/queries';
+
+const ProductList = (props) => {
   const { id: productId } = useParams();
   const { loading, data } = useQuery(QUERY_PRODUCTS, {
     variables: { id: productId },
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,28 @@
 import React from "react";
-//import { Link } from "react-router-dom";
+import { useQuery } from '@apollo/client';
+import { QUERY_PRODUCTS, QUERY_ME_BASIC } from '../utils/queries';
+import ProductList from "../components/ProductList";
+import Auth from '../utils/auth';
 
 const Home = () => {
+
+  const loggedIn = Auth.loggedIn();
+  const { loading, data } = useQuery(QUERY_PRODUCTS);
+  const products = data?.products || [];
+  console.log(products);
+
     return (
-      <div className="container">
-       <h1>hello world</h1>
+      <main>
+        <div className='flex-row justify-space-between'>
+        <div className="col-12 mb-3 ">
+        {loading ? (
+        <div>Loading...</div>
+      ) : (
+      <ProductList products={products} title="Welcome to the Garage Sale" />
+      )}
       </div>
+      </div>
+      </main>
     );
   };
   
