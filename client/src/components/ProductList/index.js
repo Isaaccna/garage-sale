3fb574feeccb82110bcaa6848d34cb822224325b
prestonwfoote <prestonwfoote@gmail.com>
2022-03-17 01:26:22 -0400

    quicky

---
+++ client/src/components/ProductList/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { Link } from "react-router-dom";
+// import CommentList from '../components/CommentList';
+// import CommentForm from '../components/CommentForm';
+import Auth from '../utils/auth';
+import { useQuery } from '@apollo/client';
+import { QUERY_PRODUCT } from '../utils/queries';
+const SingleProduct = (props) => {
+  const { id: productId } = useParams();
+  const { loading, data } = useQuery(QUERY_PRODUCT, {
+    variables: { id: productId },
+  });
+  const product = data?.product || {};
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      <div className="card mb-3">
+        <p className="card-header">
+          <span style={{ fontWeight: 700 }} className="text-light">
+            {product.username}
+          </span>{' '}
+          Posted on {product.createdAt}
+        </p>
+        <div className="card-body">
+          <p>{product.description}</p>
+        </div>
+      </div>
+      </div>
+  );
+
+};
+export default ProductList;
