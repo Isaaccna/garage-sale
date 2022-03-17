d63d2f480da29085893e3a941795ac1ba7c28225
prestonwfoote <prestonwfoote@gmail.com>
2022-03-16 22:51:34 -0400

    update comment model, NOT SCHEMA

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -1,39 +1,6 @@
 const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
-const ReactionSchema = new Schema(
-  {
-      reactionId: {
-          type: Schema.Types.ObjectId,
-          default: () => new Types.ObjectId()
-      },
-
-      reactionBody: {
-          type: String,
-          required: true,
-          minLength: 1,
-          maxLength: 280
-      },
-
-      username: {
-          type: String,
-          required: true,
-        },
-      
-       createdAt: {
-          type: Date,
-          default: Date.now,
-          get: createdAtVal => dateFormat(createdAtVal)
-        },
-
-  },
-  {
-      toJSON: {
-        getters: true,
-        virtuals: true,
-      }
-    }
-)
 
 const CommentSchema = new Schema(
   {
@@ -52,7 +19,6 @@ const CommentSchema = new Schema(
       get: timestamp => dateFormat(timestamp)
     },
 
-    reactions: [ReactionSchema]
   },
   {
     toJSON: {
@@ -61,9 +27,6 @@ const CommentSchema = new Schema(
   }
 );
 
-CommentSchema.virtual('reactionCount').get(function() {
-  return this.reactions.length;
-})
 
 const Comment = model('Comment', CommentSchema)
 
