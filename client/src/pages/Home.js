987785ba60b0571b625e75917b4bd28c2675e050
Isaaccna <isaac.andrade1231@gmail.com>
2022-03-22 23:37:21 -0400

    home page updated but the mock seed isnt working

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,21 +6,22 @@ import Auth from '../utils/auth';
 
 const Home = () => {
 
-  const loggedIn = Auth.loggedIn();
+  // use useQuery hook to make query request
   const { loading, data } = useQuery(QUERY_PRODUCTS);
+
   const products = data?.products || [];
-  console.log(products);
+console.log(products);
 
     return (
       <main>
-        <div className='flex-row justify-space-between'>
-        <div className="col-12 mb-3 ">
+       <div className='flex-row justify-space-between'>
+        <div className='col-12 mb-3'>
         {loading ? (
-        <div>Loading...</div>
-      ) : (
-      <ProductList products={products} title="Welcome to the Garage Sale" />
-      )}
-      </div>
+          <div>Loading...</div>
+        ) : (
+          <ProductList products={products} />
+        )}
+        </div>
       </div>
       </main>
     );
